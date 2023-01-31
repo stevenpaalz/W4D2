5e70529c1c72b1342832a20811a9bce98397b104
Steven Paalz <stevenpaalz@gmail.com>
2023-01-31 16:08:41 -0500

    fix git issue

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -1,48 +1,48 @@
 class Employee
-
     attr_reader :salary, :title, :name, :boss
-
+  
     def initialize(name, salary, title, boss = nil)
-        @name = name
-        @salary = salary
-        @title = title
-        @boss = boss
+      @name = name
+      @salary = salary
+      @title = title
+      @boss = boss
     end
-
+  
     def bonus(multiplier)
-        @salary * multiplier
+      @salary * multiplier
     end
-
-end
-
-class Manager < Employee
-
+  end
+  
+  class Manager < Employee
     def initialize(name, salary, title, sub_employees, boss = nil)
-        super(name, salary, title, boss)
-        @sub_employees = sub_employees
+      super(name, salary, title, boss)
+      @sub_employees = sub_employees
     end
-
+  
     def bonus(multiplier)
-        multiplier * sub_salaries
+      multiplier * sub_salaries
     end
-
+  
     protected
-
+  
     def get_sub_employees
-        
-    end
-
-    def sub_salaries
-
-
-
-        
-        total_salaries = 0
-        @sub_employees.each do |employee|
-            if employee.is_a? (Manager)
-                employee.sub_employees
-            total_salaries += employee.salary
+      result = []
+      @sub_employees.each do |employee|
+        if !employee.is_a?(Manager)
+          result << employee
+        else
+          result << employee
+          result += employee.get_sub_employees
         end
-        total_salaries
+      end
+      result
     end
-end
+  
+    def sub_salaries
+      total_salaries = 0
+      get_sub_employees.each do |employee|
+        total_salaries += employee.salary
+      end
+      total_salaries
+    end
+  end
